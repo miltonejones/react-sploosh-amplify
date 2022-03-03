1a8480508fd8f3e7e5ee9192a28a21a336226e49
miltonejones <miltonejones@gmail.com>
2022-03-03 02:25:04 -0600

    1 file modified

--- src/connector/DbConnector.js
+++ src/connector/DbConnector.js
@@ -3,6 +3,6 @@ import React from 'react';
 const API_ENDPOINT = 'https://3bax4cg6w7.execute-api.us-east-1.amazonaws.com';
 
 export const getVideos = async (page) => {
-  const response = await fetch(API_ENDPOINT + `/video/${page}`);
+  const response = await fetch(API_ENDPOINT + `/videos/${page}`);
   return await response.json();
 };
