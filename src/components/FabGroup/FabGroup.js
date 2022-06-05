008f7cb0574563e2e527ee26f8c2bbbcc51b5fb2
miltonejones <miltonejones@gmail.com>
2022-06-05 01:58:02 -0500

    adding menu backdrop

--- src/components/FabGroup/FabGroup.js
+++ src/components/FabGroup/FabGroup.js
@@ -1,6 +1,15 @@
 import React from 'react'; 
-import { Box, IconButton, Fab } from '@mui/material'; 
+import { Box, IconButton, Fab, styled } from '@mui/material'; 
 
+const Backdrop = styled('div')(({ shown }) => ({
+  width: '100vw',
+  height: '100vh',
+  backgroundColor: 'rgba(0,0,0,0.2)',
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  display: shown ? 'block' : 'none'
+}))
 
 export default function FabGroup ({ 
   icon,
@@ -14,7 +23,8 @@ export default function FabGroup ({
     position: 'fixed',
     bottom: 20,
     right: 20,
-    zIndex: 1000
+    zIndex: 1000,
+    backgroundColor: 'white' 
   }
   const style = {
     position: 'absolute',
@@ -29,28 +39,43 @@ export default function FabGroup ({
     transform: 'rotate(180deg)'
   }
 const css = on ? over : style;
+ 
 
-return <Box
+return <><Box
         style={boxStyle}
-        onMouseLeave={() =>setOn(false)}
-        onMouseOver={() => setOn(true)}>
+        onMouseLeave={() => setOn(false)}
+        onMouseMove={() => setOn(true)}>
   {buttons.map((button, i) => {
     const bottom = !on ? 20 : ((i + 2) * 48) - 8;
     const buttonStyle = {
       ...style,
       backgroundColor: 'white',
       boxShadow: 'var(--card-shadow)',
-      transition: 'bottom 0.2s linear',
+      transition: 'bottom 0.2s ease-in',
       right: 28,
       bottom 
     }
-    return <IconButton color={button.color} style={buttonStyle} onClick={button.onClick}>{button.icon}</IconButton>
+    return <IconButton color={button.color} style={buttonStyle} onClick={(w) => {
+      setOn(false);
+      button.onClick(w);
+    }}>{button.icon}</IconButton>
   })}
   <Fab style={css} color={color}
-    onClick={() => mainClick && mainClick()}
+    onClick={() => {
+      setOn(false);
+      mainClick && mainClick();
+    }}
     >
    {icon}
   </Fab>
 </Box>
+<Backdrop 
+  shown={on}
+  onMouseOver={() => setOn(true)}
+  onMouseDown={(e) => {
+    console.log ({ e });
+    setOn(false);
+  }} />
+</>
 }
 
