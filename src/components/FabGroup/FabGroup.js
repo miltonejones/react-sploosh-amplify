14e2d57ce158353d52dbafdd9505721bc6b42f25
miltonejones <miltonejones@gmail.com>
2022-06-05 23:23:01 -0500

    adding better tabs

--- src/components/FabGroup/FabGroup.js
+++ src/components/FabGroup/FabGroup.js
@@ -5,7 +5,7 @@ const Backdrop = styled('div')(({ shown }) => ({
   width: '100vw',
   height: '100vh',
   backgroundColor: 'rgba(0,0,0,0.2)',
-  position: 'absolute',
+  position: 'fixed',
   top: 0,
   left: 0,
   display: shown ? 'block' : 'none'
